3efe4ed36a07c2adb572e38a5208a3a388bbbdc5
Анатолий <Chepanov199287@gmail.com>
2023-03-28 08:50:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_4_3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            foreach (var item in arr)
+            {
+                if (item > 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
